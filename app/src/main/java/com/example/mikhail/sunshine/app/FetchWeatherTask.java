3cc207a261324085a3c391a3508d8b060e561572
MikhailSkiy <Mikhail.Skiy@gmail.com>
2015-02-28 17:00:23 +0100

    Changed ForecastFragment

--- app/src/main/java/com/example/mikhail/sunshine/app/FetchWeatherTask.java
+++ app/src/main/java/com/example/mikhail/sunshine/app/FetchWeatherTask.java
@@ -51,9 +51,10 @@ public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
     private ArrayAdapter<String> mForecastAdapter;
     private final Context mContext;
 
-    public FetchWeatherTask(Context context, ArrayAdapter<String> forecastAdapter) {
+
+
+    public FetchWeatherTask(Context context) {
         mContext = context;
-        mForecastAdapter = forecastAdapter;
     }
 
     private boolean DEBUG = true;
